20866dae9035ad8f4dd60b3855344ac75a01621e
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-05-23 21:53:37 -0600

    Saldo y Deposito Working ok

--- cajero.js
+++ cajero.js
@@ -1,14 +1,15 @@
 // Cajero Automático
 // JavaScript code for cajero screen
 
+const formDeposito = document.querySelector('#formDeposito')
 
-const usuario = sessionStorage.getItem('usuario');
-const saldo = sessionStorage.getItem('saldo');
+const usuario = sessionStorage.getItem('usuario'); //El usuario es constante
+let saldo = sessionStorage.getItem('saldo'); //Variable saldo Global para este JS
 
 console.log('usuario: ' + usuario);
 console.log('saldo: ' + saldo);
 
-document.getElementById("bienvenido").innerHTML = 'Bienvenido ' + usuario;
+document.getElementById("bienvenido").innerHTML = 'Bienvenido ' + usuario; // Crea mensaje de bienvenida
 
 
 function consultarSaldo() {
@@ -17,29 +18,23 @@ function consultarSaldo() {
     displaySaldo.classList.remove('escondido');
 }
 
-function calculaDeposito(){
-    cantidad = document.querySelector('depositoCant').value;
-    nuevoSaldo = document.querySelector('nuevoSaldo');
-    nuevoSaldo.innerHTML = 'Tu nuevo saldo es: ' + parseInt(cantidad + saldo); 
-}
 
-
-/* function mostrarSaldo(usuario){
-    let saldoVar = document.getElementById(`consultarSaldo`)
-    saldoVar.classList.remove('escondido')
-    saldoVar.classList.add('saldo')
-
-    setTimeout(()=>{
-        error.classList.remove('saldo')
-        error.classList.add('escondido')
-    }, 5000)
-} */
-
-
-/* formulario.addEventListener('submit', (evento)=>{
+formDeposito.addEventListener('submit', (evento)=>{
 
     evento.preventDefault();
 
-    mostrarSaldo('Mali')
+    let cantidad = document.querySelector('#depositoCant').value;
+    console.log(cantidad);
+    nuevoSaldo = document.querySelector('#nuevoSaldo');
+    saldo = parseInt(cantidad) + parseInt(saldo);
+    nuevoSaldo.innerHTML = 'Tu nuevo saldo es: ' + saldo;
+    nuevoSaldo.classList.remove('escondido');
+})
 
-}) */
+
+/* Al seleccionar consultar saldo, debe mostrar en pantalla el saldo actual de la cuenta
+Al seleccionar ingresar monto, el usuario debe escribir el monto a ingresar. Al ingresar el monto, debe mostrarle al usuario el monto ingresado y el nuevo saldo total.
+Al seleccionar retirar monto, el usuario debe escribir el monto a retirar. Al retirar el monto, debe mostrarle al usuario el monto retirado y el nuevo saldo total.
+
+Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10. Es necesario hacer las validaciones pertinentes para que no se rompa esta regla de negocio.
+About */
